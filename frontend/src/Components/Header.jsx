11736c9e2c33192cf0b05e953e4f10d14d54ae3c
Sarthak Jasuja <sarthakjasuja@icloud.com>
2025-07-27 02:32:11 +0530

    update

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap rounded-2xl px-6 md:px-10 lg:px-20'>
@@ -12,12 +11,17 @@ const Header = () => {
             <div>
                 <p>your ultimate travel itinerary planner!</p>
             </div>
-            <a href='#MainJourney' className='flex text-white bg-black px-8 py-3 rounded-full m-auto md:m-0 hover:scale-105 transition-all duration-300'>Plan Journey
+            <a href='' className='flex text-white bg-black px-8 py-3 rounded-full text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>Plan Journey
                 <img src='./src/assets/arrowRight.png' alt='arrow' className='w-6 h-6 cursor-pointer'/>
             </a>
             
         </div>
         {/* right side */}
+        <div className="relative md:w-1/2 flex justify-center items-end">
+            <img src="./src/assets/headerImage.jpg" alt="header" className="w-full max-w-full object-contain z-[-20] md:absolute md:bottom-0 md:h-[90%] md:z-auto"/>
+        </div>
+
+
     </div>
   )
 }
